c4a87f794b45e87f263c5eae7f9fb72e054f3f78
MaxSosnovskyi <sosnovskyimaks@gmail.com>
2024-05-12 12:34:03 +0300

    configured drf spectacular in settings.py

--- socmedapi/socmedapi/settings.py
+++ socmedapi/socmedapi/settings.py
@@ -132,5 +132,14 @@ STATIC_ROOT = '/vol/web/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Social Media for Melomaniacs',
+    'COMPONENT_SPLIT_REQUEST': True,
+}
+
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_BACKEND', 'redis://redis:6379/0')
