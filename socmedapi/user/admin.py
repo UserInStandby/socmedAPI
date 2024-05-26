0b5ea5208cc2b80b5c389e8f99955a0a8d884918
UserInStandby <maxym.sosnovskyi@gmail.com>
2024-05-26 21:02:02 +0300

    configured admin for the user model.

--- socmedapi/user/admin.py
+++ socmedapi/user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import CustomUser
+
 # Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    """Configuring admin panel for the User model."""
+
+    list_display = ["email", "created_at", "is_active", "is_staff", "is_superuser"]
+    fieldsets = [
+        (
+            "Data",
+            {
+                "fields": ["email", "is_staff", "is_superuser"],
+            },
+        ),
+        (
+            "Meta",
+            {
+                "classes": ["collapse"],
+                "fields": ["created_at", "is_active"],
+            },
+        ),
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
